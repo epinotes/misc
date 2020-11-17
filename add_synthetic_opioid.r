e7eb922c51d7d4be8d97266abe138658f2dae4df
Mamadou Ndiaye <epinotes@gmail.com>
2020-11-17 13:00:07 -0800

    Update add_synthetic_opioid.r

--- add_synthetic_opioid.r
+++ add_synthetic_opioid.r
@@ -1,6 +1,27 @@
 add_synthetic_opioid <- function(data, diag_ecode_col) {
   
   requireNamespace("dplyr", quietly = T)
+  
+  icd_new_diag <- function(data, expr, colvec, ignore.case = T, perl = T) {
+
+  requireNamespace("dplyr", quietly = T)
+
+ 
+  # assign '1' if the regular expression matched
+  f1 <- function(x) grepl(expr, x, ignore.case = ignore.case, perl = perl)
+  # any 1 in the diagnosis field suffices
+  f2 <- function(x){
+    sign(rowSums(x, na.rm = TRUE))
+  }
+
+  data %>% as_tibble() %>%
+    select(all_of({{colvec}})) %>%
+    mutate_all(as.character) %>%
+    purrr::map_dfr(f1) %>%
+    transmute(new_diag = f2(.)) %>%
+    flatten_dbl()
+}
+
 
   fentanyl_icd10cm_ <- "T4041(A|$)"
   
