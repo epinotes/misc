c9ee6b16844f52dcb3b419dfc68f5bb60e7a0e47
Mamadou Ndiaye <epinotes@gmail.com>
2020-12-04 08:34:37 -0800

    Create add_wilson_ci.r

---
+++ add_wilson_ci.r
@@ -0,0 +1,24 @@
+add_wilson_ci <- function(data, count, population, f = 10000, round = 2, level = 0.95 ){
+  
+  # confidence intervals with Wilson method 
+  #simpler than the direct method 
+  
+  # Rothman, Kenneth J. 2012. Epidemiology: An Introduction. New York: Oxford University Press.
+
+  requireNamespace("dplyr", quietly = T)
+  
+  ###
+  a <- data %>% pull({{count}})
+  N <- data %>% pull({{population}})
+  p <- a/N
+  z <- -qnorm((1-level)/2)
+  moe <-((z/sqrt(N))*sqrt(p*(1-p) + z^2/(4*N)))/(1+z^2/N)
+  center <-(p+z^2/(2*N))/(1+z^2/N)
+  ll <- round((center-moe)*f, round)
+  ul <- round((center+moe)*f, round)
+  
+  data %>% mutate(rate = round(p*f, round),
+                  lower_rate = ll,
+                  upper_rate = ul)
+  
+  }
