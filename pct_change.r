7a631ecd197d04a6102952eb3aed018081a4c2ea
Mamadou Ndiaye <epinotes@gmail.com>
2021-02-09 18:02:39 -0800

    Update pct_change.r
    remove scales::percent

--- pct_change.r
+++ pct_change.r
@@ -1,4 +1,4 @@
 pct_change <- function(prop, n = 1){
   
-  scales::percent((prop - dplyr::lag(prop, n = n))/dplyr::lag(prop, n = n))  
+ 100*(prop - dplyr::lag(prop, n = n))/dplyr::lag(prop, n = n)  
 }
