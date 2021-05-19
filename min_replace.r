41603c3dc7a87bbe1b0f4c506375701980ae7c7c
Mamadou Ndiaye <epinotes@gmail.com>
2021-05-18 17:49:14 -0700

    Update min_replace.r
    removed unnecessary parentheses

--- min_replace.r
+++ min_replace.r
@@ -1,19 +1,12 @@
 min_replace <- function(data, cond_var, min_value = 1, max_value = 9, replacement, ...) {
 
   # ... variables with values to replace with with 'replacement'
-
   # no quotations marks or vector of numeric indices
-
+  
   sel <- quos(...)
 
   data %>%
-
     mutate(
-
       across(c(!!!sel, {{cond_var}}),
-
-             function(x) (ifelse(between(({{cond_var}}), min_value, max_value), replacement, x)))
-
-    )
-
+             function(x) ifelse(between({{cond_var}}, min_value, max_value), replacement, x)))
 }
