2c6bf34468a4ce1dadfa3cd0619bf25393917386
Mamadou Ndiaye <epinotes@gmail.com>
2021-12-14 09:57:40 -0800

    Update svip_fatal_injury.R
    some "|" were missing from the regular expressions.

--- svip_fatal_injury.R
+++ svip_fatal_injury.R
@@ -7,7 +7,7 @@ svip_fatal_injury <- function(data, underly) {
 
 # "V01–Y36, Y85–Y87, Y89, U01–U03 Injury and poisoning"
 
-  icd10_valid_injury_ <- "V0[1-9]|W|X|Y[0-2]Y3[0-6]Y8[95-7]|U0[1-3]"
+  icd10_valid_injury_ <- "V0[1-9]|W|X|Y[0-2]|Y3[0-6]|Y8[95-7]|U0[1-3]"
 
   data %>%
     icd_create_indicator(
