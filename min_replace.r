33a9f875c495b0f5b8e49d9031ac729fbd14bc79
Mamadou Ndiaye <epinotes@gmail.com>
2022-08-11 21:10:56 -0700

    Update min_replace.r

--- min_replace.r
+++ min_replace.r
@@ -3,10 +3,10 @@ min_replace <- function(data, cond_var, min_value = 1, max_value = 9, replacemen
   # ... variables with values to replace with with 'replacement'
   # no quotations marks or vector of numeric indices
   
-  sel <- c({{cond_var}}, quos(...))
+  sel <- c(quos(...))
 
   data %>%
     mutate(
-      across(c(!!!sel),
+      across(c(!!!sel, {{cond_var}}),
              function(x) ifelse(between({{cond_var}}, min_value, max_value), replacement, x)))
 }
